2a767c0cb9f9470cfaad4080c66a7331e48ec146
Sondre Heggernes Padøy <sondrehp@stud.ntnu.no>
2016-01-17 12:21:10 +0100

    Format code

--- js/functions.js
+++ js/functions.js
@@ -1,28 +1,28 @@
 function statusIdToText(id) {
-   //Insert code here
-   if(id==0) {
-   	return 'Offline';
-   }
-   else if(id==1) {
-   	return 'Available';
-   }
-   else if(id==2) {
-   	return 'Away';
-   }
-   else if(id==3){
-   	return 'Occupied';
-   }
+   	if(id==0) {
+   		return 'Offline';
+   	}
+   	else if(id==1) {
+   		return 'Available';
+   	}
+   	else if(id==2) {
+   		return 'Away';
+   	}
+   	else if(id==3){
+   		return 'Occupied';
+   	}
 }
+
 function timestampToTimeOfDay(timestamp) {
 	var a = new Date(UNIX_timestamp * 1000);
-  var hour = a.getHours();
-  if(hour<10){
-  hour= '0'+hour;
+  	var hour = a.getHours();
+  	if(hour<10){
+  		hour= '0'+hour;
+  	}
+  	var min = a.getMinutes();
+  	if(min<10){
+  		min= '0'+min;
+  	}
+  	var time =  hour + ':' + min;
+  	return time;
   }
-  var min = a.getMinutes();
-  if(min<10){
-  min= '0'+min;
-  }
-  var time =  hour + ':' + min;
-  return time
-}
