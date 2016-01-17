977779075cab215977bd3f0ca3d6518d2269f376
Sondre Heggernes Padøy <sondrehp@stud.ntnu.no>
2016-01-17 11:54:16 +0100

    Map status id to text

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,19 @@
 function statusIdToText(id) {
    //Insert code here
+   if(id==0) {
+   	return 'Offline';
+   }
+   else if(id==1) {
+   	return 'Available';
+   }
+   else if(id==2) {
+   	return 'Away';
+   }
+   else if(id==3){
+   	return 'Occupied';
+   }
 }
+document.write( statusIsToText(2) )
 
 function timestampToTimeOfDay(timestamp) {
 	//Insert code here
