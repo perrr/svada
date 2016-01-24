ad3fda3f0813a82137b82dd5e282867e49af3298
perrr <perod@stud.ntnu.no>
2016-01-24 13:59:36 +0100

    Start using mysqli

--- db.php
+++ db.php
@@ -1,6 +1,14 @@
 <?php
-if (!$dbh=mysql_connect('localhost','root',''))
-	die("Could not connect to database.");
-mysql_select_db('svada');
-mysql_query('SET NAMES utf8');
+//if (!$dbh=mysql_connect('localhost','root',''))
+//	die("Could not connect to database.");
+//mysql_select_db('svada');
+//mysql_query('SET NAMES utf8');
+
+function getConnection() {
+	if (!$connection=mysqli_connect('localhost', 'root', '', 'svada'))
+		die("Could not connect to database.");
+	mysqli_query($connection, 'SET NAMES utf8');
+	return $connection;
+}
+
 ?>
--- util.php
+++ util.php
@@ -1,8 +1,12 @@
 <?php
+//Connect to database
+require('db.php');
+
+$connection = getConnection();
 
 function sqlToJson($sql) {
 	$json = array();
-	while ($row = mysql_fetch_assoc($sql)) {
+	while ($row = mysqli_fetch_assoc($sql)) {
 		$json[] = $row;
 	}
 	echo json_encode($json, JSON_NUMERIC_CHECK);
@@ -14,6 +18,11 @@ function escapeArray($array){
 	 	$array[$key] = mysql_real_escape_string($array[$key]);
 	 }
 	 return $array;
+}
+
+function query($query) {
+	global $connection;
+	return mysqli_query($connection, $query);
 }
 
 ?>
