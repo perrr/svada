ce6fb2b0f0a9fcea1caba2f3320afa5ae0313051
perrr <perod@stud.ntnu.no>
2016-01-24 10:31:01 +0100

    Add characters to avoid data.php breaking

--- data.php
+++ data.php
@@ -20,12 +20,11 @@ function getOnlineUsers() {
 	echo sqlToJson($onlineUsers);
 }
 
-
-
 function setStatusMessage($userid, $status_message){
 	mysql_query("UPDATE user 
 	SET status_message='$status_message'
 	WHERE id='$userid'");
+}
 
 function setStatus($userId, $status) {
 	mysql_query("UPDATE user SET status = $status WHERE id = $userId");
@@ -58,3 +57,5 @@ elseif($_GET['action'] == 'getAllUsers') {
 elseif($_GET['action'] == 'editMessage') {
 	editMessage($_GET['message'], $_GET['content']);
 }
+
+?>
