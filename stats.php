c7549ca579ee4c3c3b96351821900d109ffdf477
perrr <perod@stud.ntnu.no>
2016-03-04 16:48:24 +0100

    Print inside HTML

--- stats.php
+++ stats.php
@@ -15,6 +15,8 @@ mb_internal_encoding('UTF-8');
 //For convenience, store session in a variable with a shorter name
 $user = $_SESSION['user'];
 
+$content = array(); // An array containing all we wish to print
+
 $emoticonSql = getQuery("SELECT shortcut FROM emoticon");
 $shortcuts = [];
 while ($row = mysqli_fetch_assoc($emoticonSql)) {
@@ -35,7 +37,8 @@ function isEmoticon($word, $shortcuts) {
 
 function printPercentage($number, $total) {
 	$perc = ($number / $total) * 100;
-	echo ' ('.round($perc, 2).' %)';
+	global $content;
+	$content[] = ' ('.round($perc, 2).' %)';
 }
 
 function mostUsedWordsAndEmoticons($user, $shortcuts) {
@@ -78,13 +81,14 @@ function mostUsedWordsAndEmoticons($user, $shortcuts) {
 }
 
 function printWordList($words, $percent) {
+	global $content;
 	$count = 0;
 	foreach ($words as $k => $v) {
 		if ($percent)
-			echo mb_convert_case($k, MB_CASE_TITLE).' '.round($v * 100, 2).' %';
+			$content[] = mb_convert_case($k, MB_CASE_TITLE).' '.round($v * 100, 2).' %';
 		else
-			echo mb_convert_case($k, MB_CASE_TITLE).' '.$v;
-		echo '<br>';
+			$content[] = mb_convert_case($k, MB_CASE_TITLE).' '.$v;
+		$content[] = '<br>';
 		$count++;
 		if ($count == 10) {
 			break;
@@ -92,81 +96,81 @@ function printWordList($words, $percent) {
 	}
 }
 
-echo 'STATS<br><br>';
+$content[] = 'STATS<br><br>';
 
 //Database queries
 $messagesTable = mysqli_fetch_assoc(getQuery("SELECT COUNT(*) FROM message"));
 $messages = $messagesTable['COUNT(*)'];
-echo 'Number of messages: ';
-echo $messages;
+$content[] = 'Number of messages: ';
+$content[] = $messages;
 $users = getQuery("SELECT id, username FROM user");
 
-echo '<br><br>';
+$content[] = '<br><br>';
 foreach ($users as $user) {
 	$id = $user['id'];
 	$userMessagesTable = mysqli_fetch_assoc(getQuery("SELECT COUNT(*) FROM message WHERE author = $id"));
 	$userMessages = $userMessagesTable['COUNT(*)'];
-	echo $user['username'].': ';
-	echo $userMessages;
+	$content[] = $user['username'].': ';
+	$content[] = $userMessages;
 	printPercentage($userMessages, $messages);
-	echo '<br>';
+	$content[] = '<br>';
 }
 
-echo '<br>';
+$content[] = '<br>';
 $skypeTable = mysqli_fetch_assoc(getQuery("SELECT COUNT(*) FROM message WHERE skype = 1"));
 $skype = $skypeTable['COUNT(*)'];
-echo 'Messages from Skype: ';
-echo $skype;
+$content[] = 'Messages from Skype: ';
+$content[] = $skype;
 printPercentage($skype, $messages);
 
-echo '<br>';
+$content[] = '<br>';
 $notSkypeTable = mysqli_fetch_assoc(getQuery("SELECT COUNT(*) FROM message WHERE skype = 0"));
 $notSkype = $notSkypeTable['COUNT(*)'];
-echo 'Messages not from Skype: ';
-echo $notSkype;
+$content[] = 'Messages not from Skype: ';
+$content[] = $notSkype;
 printPercentage($notSkype, $messages);
 
-echo '<br><br>';
+$content[] = '<br><br>';
 $lengthTable = mysqli_fetch_assoc(getQuery("SELECT AVG(LENGTH(content)) FROM message"));
 $length = $lengthTable['AVG(LENGTH(content))'];
-echo 'Average message length: ';
-echo round($length, 2);
+$content[] = 'Average message length: ';
+$content[] = round($length, 2);
 
-echo '<br><br>';
+$content[] = '<br><br>';
 foreach ($users as $user) {
 	$id = $user['id'];
 	$userLengthTable = mysqli_fetch_assoc(getQuery("SELECT AVG(LENGTH(content)) FROM message WHERE author = $id"));
 	$userLength = $userLengthTable['AVG(LENGTH(content))'];
-	echo $user['username'].': ';
-	echo round($userLength, 2);
-	echo '<br>';
+	$content[] = $user['username'].': ';
+	$content[] = round($userLength, 2);
+	$content[] = '<br>';
 }
 
 list($mostUsedWords, $mostUsedEmoticons, $numWordsTotal, $numEmoticonsTotal) = mostUsedWordsAndEmoticons(null, $shortcuts);
 $numWordsUnique = sizeof($mostUsedWords);
-echo '<br>Total number of words: '.$numWordsTotal.'<br>';
-echo '<br>Total number of unique words: '.$numWordsUnique.'<br>';
-echo '<br>Most used words:<br>';
+$content[] = '<br>Total number of words: '.$numWordsTotal.'<br>';
+$content[] = '<br>Total number of unique words: '.$numWordsUnique.'<br>';
+$content[] = '<br>Most used words:<br>';
 printWordList($mostUsedWords, false);
 $numEmoticonsUnique = sizeof($mostUsedEmoticons);
-echo '<br>Total number of emoticons: '.$numEmoticonsTotal.'<br>';
-echo '<br>Total number of unique emoticons: '.$numEmoticonsUnique.'<br>';
-echo '<br>Most used emoticons:<br>';
+$content[] = '<br>Total number of emoticons: '.$numEmoticonsTotal.'<br>';
+$content[] = '<br>Total number of unique emoticons: '.$numEmoticonsUnique.'<br>';
+$content[] = '<br>Most used emoticons:<br>';
 printWordList($mostUsedEmoticons, false);
 foreach ($users as $user) {
 	list($userWords, $userEmoticons, $numWordsUser, $numEmoticonsUser) = mostUsedWordsAndEmoticons($user['id'], $shortcuts);
 	$numWordsUserUnique = sizeof($userWords);
-	echo '<br>Number of words for '.$user['username'].': '.$numWordsUser.'<br>';
-	echo '<br>Number of unique words for '.$user['username'].': '.$numWordsUserUnique.'<br>';
-	echo '<br>Most used words for '.$user['username'].':<br>';
+	$content[] = '<br>Number of words for '.$user['username'].': '.$numWordsUser.'<br>';
+	$content[] = '<br>Number of unique words for '.$user['username'].': '.$numWordsUserUnique.'<br>';
+	$content[] = '<br>Most used words for '.$user['username'].':<br>';
 	printWordList($userWords, false);
 	$numEmoticonsUserUnique = sizeof($userEmoticons);
-	echo '<br>Number of emoticons for '.$user['username'].': '.$numEmoticonsUser.'<br>';
-	echo '<br>Number of unique emoticons for '.$user['username'].': '.$numEmoticonsUserUnique.'<br>';
-	echo '<br>Most used emoticons for '.$user['username'].':<br>';
+	$content[] = '<br>Number of emoticons for '.$user['username'].': '.$numEmoticonsUser.'<br>';
+	$content[] = '<br>Number of unique emoticons for '.$user['username'].': '.$numEmoticonsUserUnique.'<br>';
+	$content[] = '<br>Most used emoticons for '.$user['username'].':<br>';
 	printWordList($userEmoticons, false);
 	
-	echo '<br>Relatively most used words for '.$user['username'].':<br>';
+	$content[] = '<br>Relatively most used words for '.$user['username'].':<br>';
 	$relWords = array();
 	foreach ($userWords as $k => $v) {
 		$relWords[$k] = ($v / $numWordsUser) / ($mostUsedWords[$k] / $numWordsTotal);
@@ -174,7 +178,7 @@ foreach ($users as $user) {
 	asort($relWords);
 	$relWords = array_reverse($relWords);
 	printWordList($relWords, true);
-	echo '<br>Relatively most used emoticons for '.$user['username'].':<br>';
+	$content[] = '<br>Relatively most used emoticons for '.$user['username'].':<br>';
 	$relEmoticons = array();
 	foreach ($userEmoticons as $k => $v) {
 		$relEmoticons[$k] = ($v / $numEmoticonsUser) / ($mostUsedEmoticons[$k] / $numEmoticonsTotal);
@@ -204,7 +208,11 @@ mysqli_close($connection);
 		<link href="css/styles.css" rel="stylesheet">
 	</head>
 	<body>
-		
+		<?php
+		foreach ($content as $text) {
+			echo $text;
+		}
+		?>
 		<!-- Scripts -->
 		<script src="js/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
