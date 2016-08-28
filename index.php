a0f676f429bcf7f0dce33a623142936dcd9a38fe
Audun <Audun_L@hotmail.com>
2016-08-28 15:20:17 +0200

    Fix broken script links in index

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if(isset($_POST['username'])){
 		<title>Svada Chat Client</title>
 		<meta name="generator" content="Bootply" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/plugins/bootstrap.min.css" rel="stylesheet">
 		<!--[if lt IE 9]>
 			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
@@ -90,7 +90,7 @@ if(isset($_POST['username'])){
 		</div>
 		
 		<!-- Scripts -->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
+		<script src="js/plugins/jquery.min.js"></script>
+		<script src="js/plugins/bootstrap.min.js"></script>
 	</body>
 </html>
