5521ee97c2a14f49919a5173425d3c01f29d0952
perrr <perod@stud.ntnu.no>
2016-09-02 15:59:11 +0200

    Specify database name in install (B61)

--- db.php
+++ db.php
@@ -6,8 +6,8 @@ function getConnection() {
 			header("Location: ./install.php"); // Redirect browser
 			exit();
 		}
-		
-		if (!$connection=mysqli_connect($dbDetails['host'], $dbDetails['username'], $dbDetails['password'], 'svada'))
+
+		if (!$connection=mysqli_connect($dbDetails['host'], $dbDetails['username'], $dbDetails['password'], $dbDetails['db_name']))
 			die("Could not connect to database.");
 		mysqli_query($connection, 'SET NAMES utf8');
 		return $connection;
