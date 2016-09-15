161145163f6c667d9aed5b5528c55fcbbc463445
perrr <perod@stud.ntnu.no>
2016-09-15 19:20:51 +0200

    Add errors to install script and a few other things

--- install.php
+++ install.php
@@ -7,108 +7,132 @@ function createIniFile($host, $username, $password, $databaseName) {
 	$content.= "\ndb_name = ".$databaseName;
 
 	mkdir("./conf");
+	mkdir("./uploads");
 	$file = fopen("./conf/settings.ini", "w");
 	fwrite($file, $content);
 	fclose($file);
 }
 
 if (isset($_POST["ip"])) {
-	//Connect to database
-	// THIS DOES NOT WORK BECAUSE WE HAVE NO CONNECTION YET, NEED TO FIX THIS
-	$connection = new mysqli($_POST["ip"], $_POST["db_user"], $_POST["db_password"]);
-	//$connection = new mysqli($connection->real_escape_string($_POST["ip"]), $connection->real_escape_string($_POST["db_user"]), $connection->real_escape_string($_POST["db_password"]));
-	if ($connection->connect_error) {
-	    die("Connection failed: " . $connection->connect_error);
+
+	if ($_POST["chat"] == "")
+		echo "Chat name cannot be empty";
+
+	elseif ($_POST["username"] == "")
+		echo "Username cannot be empty";
+
+	elseif ($_POST["display"] == "")
+		echo "Display name cannot be empty";
+
+	elseif ($_POST["password"] == "")
+		echo "Password cannot be empty";
+
+	elseif ($_POST["password2"] != $_POST["password"])
+		echo "Passwords do not match!";
+
+	else {
+		//Connect to database
+		$connection = @new mysqli($_POST["ip"], $_POST["db_user"], $_POST["db_password"]);
+
+		if ($connection->connect_error) {
+		    echo("Connection failed: " . $connection->connect_error);
+		}
+
+		else {
+			foreach($_POST as $key => &$value) {
+	    		$value = $connection->real_escape_string($value);
+			}
+
+			//Create database
+			mysqli_query($connection, "CREATE DATABASE IF NOT EXISTS ".$_POST["db_name"]) or die(mysqli_error($connection));
+			require('util.php');
+			setConnection($connection);
+			setQuery("USE ".$_POST["db_name"]);
+			//Create tables
+			setQuery("DROP TABLE IF EXISTS `chat`");
+			setQuery("CREATE TABLE `chat` (
+			  `name` varchar(20) NOT NULL,
+			  `topic` text NOT NULL,
+			  `image` int(11) DEFAULT NULL
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1");
+			setQuery('INSERT INTO `chat` (`name`, `topic`) VALUES
+			("'.$connection->real_escape_string($_POST["chat"]).'", "")');
+
+			setQuery("DROP TABLE IF EXISTS `emoticon`");
+			setQuery("CREATE TABLE `emoticon` (
+			  `id` int(11) NOT NULL AUTO_INCREMENT,
+			  `path` varchar(30) NOT NULL,
+			  `name` varchar(20) NOT NULL,
+			  `shortcut` text NOT NULL,
+			  PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1");
+
+			setQuery("DROP TABLE IF EXISTS `file`");
+			setQuery("CREATE TABLE `file` (
+			  `id` int(11) NOT NULL AUTO_INCREMENT,
+			  `path` varchar(30) NOT NULL,
+			  `uploader` int(11) NOT NULL,
+			  `timestamp` int(11) NOT NULL,
+			  PRIMARY KEY (`id`)
+			) ENGINE=MyISAM DEFAULT CHARSET=latin1");
+
+			setQuery("DROP TABLE IF EXISTS `message`");
+			setQuery("CREATE TABLE `message` (
+			  `id` int(11) NOT NULL AUTO_INCREMENT,
+			  `content` text NOT NULL,
+			  `author` int(11) NOT NULL,
+			  `timestamp` int(11) NOT NULL,
+			  `edit` int(11) NOT NULL DEFAULT '0',
+			  `skype` int(11) NOT NULL DEFAULT '0',
+			  PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1");
+
+			setQuery("DROP TABLE IF EXISTS `style`");
+			setQuery("CREATE TABLE `style` (
+			  `id` int(11) NOT NULL AUTO_INCREMENT,
+			  `name` varchar(30) NOT NULL,
+			  `css` varchar(30) NOT NULL,
+			  `markup` varchar(30) NOT NULL,
+			  PRIMARY KEY (`id`)
+			) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1");
+			setQuery("INSERT INTO `style` (`id`, `name`, `css`, `markup`) VALUES
+			(1, 'Standard', 'standard.css', 'highlight.xcode.css')");
+
+			setQuery("DROP TABLE IF EXISTS `user`");
+			setQuery("CREATE TABLE `user` (
+			  `username` varchar(20) NOT NULL,
+			  `display_name` varchar(30) NOT NULL,
+			  `id` int(11) NOT NULL AUTO_INCREMENT,
+			  `password` varchar(255) NOT NULL,
+			  `status` int(11) NOT NULL DEFAULT '0',
+			  `status_message` varchar(100) NOT NULL,
+			  `image` int(11) DEFAULT NULL,
+			  `is_typing` int(11) NOT NULL DEFAULT '0',
+			  `language` varchar(20) NOT NULL DEFAULT 'english',
+			  `mute_sounds` int(11) NOT NULL DEFAULT '0',
+			  `last_activity` int(11) NOT NULL DEFAULT '0',
+			  `style` int(11) NOT NULL DEFAULT '1',
+			  PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1");
+			setQuery('INSERT INTO `user` (`username`, `display_name`, `password`, `status_message`) VALUES 
+				("'.strtolower($connection->real_escape_string($_POST['username'])).'", "'.$connection->real_escape_string($_POST["display"]).'", "'.md5(salt($connection->real_escape_string($_POST['password']), $_POST['username'])).'", "")');
+			
+			setQuery("DROP TABLE IF EXISTS `user_session`");
+			setQuery("CREATE TABLE `user_session` (
+			  `id` int(11) NOT NULL,
+			  `token` varchar(33) DEFAULT NULL
+			) ENGINE=MyISAM DEFAULT CHARSET=latin1");
+
+			//Write to .ini file
+			createIniFile($_POST["ip"], $_POST["db_user"], $_POST["db_password"], $_POST["db_name"]);
+
+			// Redirect browser
+			header("Location: ./index.php");
+
+			//Delete this file upon completion
+			//unlink(__FILE__);
+		}
 	}
-
-	//Create database
-	mysqli_query($connection, "CREATE DATABASE IF NOT EXISTS ".$_POST["db_name"]) or die(mysqli_error($connection));
-	require('util.php');
-	setConnection($connection);
-	setQuery("USE ".$_POST["db_name"]);
-	//Create tables
-	setQuery("DROP TABLE IF EXISTS `chat`");
-	setQuery("CREATE TABLE `chat` (
-	  `name` varchar(20) NOT NULL,
-	  `topic` text NOT NULL,
-	  `image` int(11) DEFAULT NULL
-	) ENGINE=InnoDB DEFAULT CHARSET=latin1");
-	setQuery('INSERT INTO `chat` (`name`, `topic`) VALUES
-	("'.$connection->real_escape_string($_POST["chat"]).'", "")');
-
-	setQuery("DROP TABLE IF EXISTS `emoticon`");
-	setQuery("CREATE TABLE `emoticon` (
-	  `id` int(11) NOT NULL AUTO_INCREMENT,
-	  `path` varchar(30) NOT NULL,
-	  `name` varchar(20) NOT NULL,
-	  `shortcut` text NOT NULL,
-	  PRIMARY KEY (`id`)
-	) ENGINE=InnoDB DEFAULT CHARSET=latin1");
-
-	setQuery("DROP TABLE IF EXISTS `file`");
-	setQuery("CREATE TABLE `file` (
-	  `id` int(11) NOT NULL AUTO_INCREMENT,
-	  `path` varchar(30) NOT NULL,
-	  `uploader` int(11) NOT NULL,
-	  `timestamp` int(11) NOT NULL,
-	  PRIMARY KEY (`id`)
-	) ENGINE=MyISAM DEFAULT CHARSET=latin1");
-
-	setQuery("DROP TABLE IF EXISTS `message`");
-	setQuery("CREATE TABLE `message` (
-	  `id` int(11) NOT NULL AUTO_INCREMENT,
-	  `content` text NOT NULL,
-	  `author` int(11) NOT NULL,
-	  `timestamp` int(11) NOT NULL,
-	  `edit` int(11) NOT NULL DEFAULT '0',
-	  `skype` int(11) NOT NULL DEFAULT '0',
-	  PRIMARY KEY (`id`)
-	) ENGINE=InnoDB DEFAULT CHARSET=latin1");
-
-	setQuery("DROP TABLE IF EXISTS `style`");
-	setQuery("CREATE TABLE `style` (
-	  `id` int(11) NOT NULL AUTO_INCREMENT,
-	  `name` varchar(30) NOT NULL,
-	  `css` varchar(30) NOT NULL,
-	  `markup` varchar(30) NOT NULL,
-	  PRIMARY KEY (`id`)
-	) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1");
-	setQuery("INSERT INTO `style` (`id`, `name`, `css`, `markup`) VALUES
-	(1, 'Standard', 'standard.css', 'highlight.xcode.css')");
-
-	setQuery("DROP TABLE IF EXISTS `user`");
-	setQuery("CREATE TABLE `user` (
-	  `username` varchar(20) NOT NULL,
-	  `display_name` varchar(30) NOT NULL,
-	  `id` int(11) NOT NULL AUTO_INCREMENT,
-	  `password` varchar(255) NOT NULL,
-	  `status` int(11) NOT NULL DEFAULT '0',
-	  `status_message` varchar(100) NOT NULL,
-	  `image` int(11) DEFAULT NULL,
-	  `is_typing` int(11) NOT NULL DEFAULT '0',
-	  `language` varchar(20) NOT NULL DEFAULT 'english',
-	  `mute_sounds` int(11) NOT NULL DEFAULT '0',
-	  `last_activity` int(11) NOT NULL DEFAULT '0',
-	  `style` int(11) NOT NULL DEFAULT '1',
-	  PRIMARY KEY (`id`)
-	) ENGINE=InnoDB DEFAULT CHARSET=latin1");
-	setQuery('INSERT INTO `user` (`username`, `display_name`, `password`, `status_message`) VALUES 
-		("'.strtolower($connection->real_escape_string($_POST['username'])).'", "'.$connection->real_escape_string($_POST["display"]).'", "'.md5(salt($connection->real_escape_string($_POST['password']), $_POST['username'])).'", "")');
-	
-	setQuery("DROP TABLE IF EXISTS `user_session`");
-	setQuery("CREATE TABLE `user_session` (
-	  `id` int(11) NOT NULL,
-	  `token` varchar(33) DEFAULT NULL
-	) ENGINE=MyISAM DEFAULT CHARSET=latin1");
-
-	//Write to .ini file
-	createIniFile($_POST["ip"], $_POST["db_user"], $_POST["db_password"], $_POST["db_name"]);
-
-	// Redirect browser
-	header("Location: ./index.php");
-
-	//Delete this file upon completion
-	//unlink(__FILE__);
 }
 
 ?>
@@ -137,7 +161,7 @@ if (isset($_POST["ip"])) {
 			Your username: <input type="text" name="username"><br>
 			Your displayname: <input type="text" name="display"><br>
 			Your password: <input type="password" name="password"><br>
-			<!--Repeat password: <input type="text" name="password2"><br>-->
+			Repeat password: <input type="password" name="password2"><br>
 			<input type="submit">
 		</form>
 		<!-- Scripts -->
