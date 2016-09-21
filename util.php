a7d0692a3b39db4d993385631048c579a05640b9
Audun <Audun_L@hotmail.com>
2016-09-22 01:02:18 +0200

    Fix small install-breaking bug

--- util.php
+++ util.php
@@ -1,11 +1,12 @@
 <?php
 //Connect to database
 require('db.php');
-if (basename($_SERVER["SCRIPT_FILENAME"]) != 'install.php')
+if (basename($_SERVER["SCRIPT_FILENAME"]) != 'install.php') {
 	$connection = getConnection();
 
-if(!isset($_SESSION['user']))
-	renewSession();
+	if(!isset($_SESSION['user']))
+		renewSession();
+}
 
 // If user is not logged in, set English as default
 if(isset($_SESSION['user'])){
