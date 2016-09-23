814960d7d81b012c3e6c9810e053bd27050ac55f
EirikZimmerWold <eirikzimmer@hotmail.com>
2016-09-23 16:46:30 +0200

    settings now can mute sounds
    B66

--- pages/settings.php
+++ pages/settings.php
@@ -4,6 +4,13 @@ if (isset($_POST['saveSettings'])) {
 	if ($user['language'] != $_POST['language']) {
 		setQuery("UPDATE user set language = ".$_POST['language']." WHERE id = ".$user['id']);
 	}
+	if ($user['style'] != $_POST['style']) {
+		setQuery("UPDATE user set style = ".$_POST['style']." WHERE id = ".$user['id']);
+	}
+	$muteSound = isset($_POST['sound'])==true ? 1 : 0;
+	if ($user['mute_sounds'] != $muteSound) {
+		setQuery("UPDATE user set mute_sounds = ".$muteSound." WHERE id = ".$user['id']);
+	}
 	
 	//Update page
 	updateUserSession();
@@ -25,8 +32,12 @@ while ($style = mysqli_fetch_assoc($styles)) {
 	$styleOptions .= '<option value="'.$style['id'].'"'.$selected.'>'.$style['name'].'</option>';
 }
 
+$checked =($user['mute_sounds']== 1) ? ' checked' : '';
+$checkbox = '<input type="checkbox" name="sound" id="sound" '.$checked.'> ';
+
 ?>
 <h1 class="tab-header col-sm-12"><?php echo getString('settings'); ?></h1>
+<div class="form-horizontal settings-form">
   <div class="form-group dropdown-form-group">
     <label for="language" class="control-label col-sm-4"><?php echo getString('changeLanguage'); ?>:</label>
 	<div class="col-sm-8">
@@ -34,6 +45,8 @@ while ($style = mysqli_fetch_assoc($styles)) {
 			<?php echo $languageOptions; ?>
 		</select>
 	</div>
+	</div>
+	<div class="form-group dropdown-form-group">
     <label for="style" class="control-label col-sm-4"><?php echo getString('changeStyle'); ?>:</label>
 	<div class="col-sm-8">
 		<select class="form-control" id="style" name="style">
@@ -42,7 +55,12 @@ while ($style = mysqli_fetch_assoc($styles)) {
 	</div>
   </div>
   <div class="form-group">
+	<label for="muteSound" class="control-label col-sm-4"><?php echo getString('changeSound'); ?>:</label>
+	<?php echo $checkbox; ?>
+  </div>
+  <div class="form-group">
     <div class="col-sm-12">
       <button type="submit" class="btn button" name="saveSettings"><?php echo getString('save'); ?></button>
     </div>
+  </div>
   </div>
