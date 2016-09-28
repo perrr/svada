a9a27f5ce8f803d7aa29c9b5115f8f60766baab4
Audun <Audun_L@hotmail.com>
2016-09-28 22:35:43 +0200

    Give files original file name when downloading

--- download.php
+++ download.php
@@ -3,14 +3,14 @@
 session_start();
 
 require('util.php');
-global $connection;
+
 $id = $connection->real_escape_string($_GET['id']);
-$fileQuery = getQuery("SELECT path FROM file WHERE id = '$id'");
+$fileQuery = getQuery("SELECT path, name FROM file WHERE id = '$id'");
 $file = $fileQuery -> fetch_assoc();
 $path = 'uploads/' . $file['path'];
 header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary"); 
-header("Content-disposition: attachment; filename=\"" . basename($path) . "\"");
+header('Content-Transfer-Encoding: Binary'); 
+header('Content-disposition: attachment; filename="' . $file['name'] . '"');
 readfile($path);
 
 //Close connection to database
