314ebc4ccddfc5bcb724bd4079a7a8433aaacd7b
Vardark <audun_l@hotmail.com>
2016-12-30 14:49:14 +0100

    Add promises when loading search results

--- js/search.js
+++ js/search.js
@@ -9,6 +9,7 @@ function displayResults(string, caseSensitive, userId) {
 	return postData("searchMessages", { string, caseSensitive, userId }).done(function(json){
 		$("#search-num-results").html(json.length + " " + (json.length != 1 ? language["results"] : language["result"]) + ".");
 		var html = "";
+		var promises = [];
 		var currentDate = new Date(0);
 		for (var i = 0; i < json.length; i++) {
 			var thisDate = new Date(json[i].timestamp*1000);
@@ -17,18 +18,24 @@ function displayResults(string, caseSensitive, userId) {
 				currentDate = thisDate;
 			}
 			
-			var promise = parseMessage(json[i]);
-
-			$.when(promise).then(function() {
-				$("search-message" + json[i].id).html(json[i].parsedContent);
-			});
+			promises.push(parseMessage(json[i]));
 			
 			if (json[i].author > 0)
 				html += displayMessage(json[i], "search-");
 			else 
 				html += displaySystemMessage(json[i], "search-");
+			
 		}
 		$("#search-results").html(html);
+		
+		for (var i = 0; i < promises.length; i++) {
+			
+			$.when(promises[i]).then(function() {
+				var re = new RegExp("("+string+")");
+				$("#search-message" + json[i].id).html(json[i].parsedContent.replace(re,'<b>$1</b>'));
+			});
+			
+		}
 	});
 }
 
